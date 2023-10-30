6d8232c1d5f5706b8999410d27107a2c5cda7600
fulqjulio <juliancarvajal93f@gmail.com>
2023-10-29 21:16:19 -0500

    added bicicletas api

--- red_bicicletas/app.js
+++ red_bicicletas/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bicicletasRouter = require('./routes/bicicletas');
+var bicicletasAPIRouter = require('./routes/api/bicicletas');
 
 var app = express();
 
@@ -20,10 +21,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rutas
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bicicletas', bicicletasRouter);
+// Routes API
+app.use('/api/bicicletas', bicicletasAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- red_bicicletas/public/javascripts/map.js
+++ red_bicicletas/public/javascripts/map.js
@@ -4,3 +4,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici) {
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+})
