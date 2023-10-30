979b557e5eeeec4ab341e5b0ca9526c881eeb97e
fulqjulio <juliancarvajal93f@gmail.com>
2023-10-29 20:14:10 -0500

    crud made load in memory

---
+++ red_bicicletas/public/javascripts/map.js
@@ -0,0 +1,6 @@
+var map = L.map('main_map').setView([-34.6012424,-58.3861497], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map)
